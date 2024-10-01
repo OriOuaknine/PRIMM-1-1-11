caa494162fa3dfef97262617f089a5d7b5714a74
OriOuaknine <oriouak9@gmail.com>
2024-10-01 15:46:46 -0600

    Update primm_11.py

--- primm_11.py
+++ primm_11.py
@@ -6,13 +6,13 @@ Name - Date
 #this displays the number 1 plus the number 2, but doesn't include the values
 def main():
   
-    num1: int = input("Enter a number: ")
-    num1 = int(num1)
+    num1: float = float("Enter a number: ")
+    num1 = float(num1)
   
-    num2: int = input("Enter another number: ")
-    num2 = int(num2)
+    num2: float = float("Enter another number: ")
+    num2 = float(num2)
   
-    total: int = num1+num2
+    total: float = num1+num2
 
     print(f"{num1} + {num2} = {total}")
 
