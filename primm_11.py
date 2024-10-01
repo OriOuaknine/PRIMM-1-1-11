d8e861551d209ae4b7b879028f843eb835495ffd
OriOuaknine <oriouak9@gmail.com>
2024-10-01 15:39:58 -0600

    Update primm_11.py by fixing code to put the 2 number together

--- primm_11.py
+++ primm_11.py
@@ -1,13 +1,17 @@
 """
 PRIMM: 1+1 = 11
-Description of program here
+program displays the first number inputted and with the second number inputted
 Name - Date
 """
-
+#this displays the number 1 plus the number 2, but doesn't include the values
 def main():
   
     num1: int = input("Enter a number: ")
+    num1 = int(num1)
+  
     num2: int = input("Enter another number: ")
+    num2 = int(num2)
+  
     total: int = num1+num2
 
     print(f"{num1} + {num2} = {total}")
